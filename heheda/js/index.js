d042288700e97072cd9c72e9e66bbedbfa41d5b4
root <root@cvm-3iqxkwe2da225.novalocal>
2023-09-27 19:54:01 +0800

    update

--- heheda/js/index.js
+++ heheda/js/index.js
@@ -1,5 +1,6 @@
 // 获取必要参数
 let title = document.title;
+let url = window.location.href;
 if (!navigator.share) {
 	document.getElementById("cd-top").style.display = "none";
 }
@@ -8,34 +9,26 @@ if (!navigator.share) {
 if (navigator.userAgent.match(/(MSIE|rv:11\.0)/)) {
 	document.body.classList.add("is-ie");
 }
-document.onreadystatechange = function() {
-	if (document.readyState === "complete") {
-		eventHandler();
-		document.body.classList.remove("is-loading");
-	}
+window.onload = function() {
+	document.body.classList.remove("is-loading");
 }
 
 //网页飘落效果
-function eventHandler() {
-	$('body').wpSuperSnow({
-		flakes: ['./heheda/image/007.png', './heheda/image/006.png', './heheda/image/004.png',
-			'./heheda/image/005.png', './heheda/image/001.png', './heheda/image/003.png',
-			'./heheda/image/002.png', './heheda/image/008.png'
-		],
-		totalFlakes: '100',
-		zIndex: '999999999',
-		maxSize: '30',
-		maxDuration: '50',
-		useFlakeTrans: false
-	});
+let pf = new PF({
+	life: 30,
+});
+pf.init();
+pf.start();
+window.onresize = function() {
+	pf.reSize();
 }
 
 //分享设置
 function call() {
 	navigator.share({
 		title: title,
-		url: window.location.href,
-		text: '赵彤刚的工具箱'
+		url: url,
+		text: title
 	});
 }
 
@@ -51,24 +44,25 @@ if ("Notification" in window) {
 }
 
 function sendNotification() {
-	new Notification("赵彤刚的小屋", {
+	new Notification(title, {
 		body: '久违了我的朋友，欢迎您的访问！',
 		icon: './heheda/icon/128.png'
 	})
 }
 
 // 标题判断
-function istitle() {
+document.addEventListener('visibilitychange', function() {
 	if (document.hidden) {
 		//当窗口不可见
 		document.title = '(つ ェ ⊂)我藏好了哦~';
 	} else {
 		//当窗口可见
 		document.title = '(*゜ロ゜)ノ被发现了~';
-		setTimeout("document.title=title", 3000);
+		setTimeout(() => {
+			document.title = title;
+		}, 3000);
 	}
-};
-document.addEventListener('visibilitychange', istitle);
+})
 
 // 版权
 console.log("%c赵彤刚%c版权所有", "font-size:15px;padding:3px;color:white;background:#023047",
